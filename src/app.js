aee02258da31bcc7ac379a2293b73bea8a48411e
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2022-10-19 21:05:32 +0000

    Subido ya todo listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let whoList = [
+    "My wife ",
+    "My dog ",
+    "My neighbord ",
+    "My grandma ",
+    "My little sister ",
+    "The priest of my church "
+  ];
+  let who = whoList[Math.floor(Math.random() * whoList.length)];
+
+  let actionList = ["eated ", "kicked ", "broke ", "vomited ", "burned "];
+  let action = actionList[Math.floor(Math.random() * actionList.length)];
+
+  let thingList = [
+    "my clock ",
+    "my homework ",
+    "my house ",
+    "my car ",
+    "the kitchen ",
+    "my bed "
+  ];
+  let thing = thingList[Math.floor(Math.random() * thingList.length)];
+
+  let whenList = [
+    "yesterday",
+    "this morning",
+    "while I was getting a shower",
+    "during the diner",
+    "last night",
+    "during my nap",
+    "in my dreams"
+  ];
+  let when = whenList[Math.floor(Math.random() * whenList.length)];
+
+  let excuse = who + action + thing + when;
+
+  document.write(excuse);
   console.log("Hello Rigo from the console!");
 };
