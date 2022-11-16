e27da8ef8404c568f0e4960f1672643eed3975ba
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2022-11-16 19:35:33 +0000

    Remake the challenge

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoList = ["My cat", "My dog", "My dad", "My iguana", "My grandfather"];
+  let actionList = ["ate", "watched", "broke", "pee", "vomit"];
+  let whatList = ["my bed", "the kitchen", "my house", "my cellphone"];
+  let whenList = ["yestaday", "last night"];
+
+  let who = whoList[Math.floor(Math.random() * whoList.length)];
+  let action = actionList[Math.floor(Math.random() * actionList.length)];
+  let what = whatList[Math.floor(Math.random() * whatList.length)];
+  let when = whenList[Math.floor(Math.random() * whenList.length)];
+
+  const excusa = document.querySelector("#js-excuse");
+  excusa.innerHTML = `${who} ${action} ${what} ${when}.`;
 };
+
+function iterador(a1, a2, a3, a4) {
+  let excusa = "";
+  let listas = [a1, a2, a3, a4];
+  for (let lista of listas) {
+    excusa += lista[Math.floor(Math.random() * lista.length)];
+  }
+}
